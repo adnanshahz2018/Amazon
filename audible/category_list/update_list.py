51631c778475a64bb8060998466d9d99e0436ec0
Adnan Shah <43321679+adnanshahz2018@users.noreply.github.com>
2021-03-13 15:38:50 +0500

    Update update_list.py

--- audible/category_list/update_list.py
+++ audible/category_list/update_list.py
@@ -1,11 +1,33 @@
 
 import json
+import pandas as pd
+from numpy import nan
 from bs4 import BeautifulSoup
 from selenium import webdriver 
 
 audible_best_sellers    = {
+    'Australia': 'https://www.amazon.com.au/gp/bestsellers/audible/ref=zg_bs_nav_0',
+    'Brazil': '',
+    'Canada': 'https://www.amazon.ca/Best-Sellers-Audible-Audiobooks/zgbs/audible/ref=zg_bs_nav_0',
+    'China': '',
+    'France': 'https://www.amazon.fr/gp/bestsellers/audible/ref=zg_bs_nav_0',
+    'Germany': 'https://www.amazon.de/gp/bestsellers/audible/ref=zg_bs_nav_0',
+    'India': 'https://www.amazon.in/gp/bestsellers/audible/ref=zg_bs_nav_0',
+    'Italy': 'https://www.amazon.it/gp/bestsellers/audible/ref=zg_bs_nav_0',
+    'Japan': 'https://www.amazon.co.jp/-/en/gp/bestsellers/audible/ref=zg_bs_nav_0',
+    # 'Mexico': '',
+    # 'Netherlands': '',
+    # 'Poland': '',
+    # 'Saudi Arabia': '',
+    # 'Singapore': '',
+    # 'Spain': '',
+    # 'Sweden': '',
+    # 'Turkey': '',
+    # 'United Arab Emirates': '',
+    'United Kingdom': 'https://www.amazon.co.uk/Best-Sellers-Audible-Audiobooks/zgbs/audible/ref=zg_bs_nav_0',
     'United States' : 'https://www.amazon.com/Best-Sellers-Audible-Audiobooks/zgbs/audible/?_encoding=UTF8&ref_=sv_adbl_subnav_ref1_2'
 }
+
 audible_categories_us   = {}
 
 
@@ -49,6 +71,20 @@ class audible:
         return cat_dict
 
 
+def selected_countries():
+    countries =  pd.read_excel('countries.xlsx', 'countries')
+    datas = countries['countries']
+    country_list = []
+    for i in datas.index:
+            if datas[i] is not nan:    
+                country_list.append(datas[i])
+    return country_list
+
+
 if __name__ == '__main__':
     audi = audible()
-    audi.category()
+    selectedcountries = selected_countries()
+    for country in audible_best_sellers:
+        if country in selectedcountries:
+            # audi.category(country)
+            print(country)
